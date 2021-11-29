eb58deb7b037bc849771a0ae3e53a1d1c3034ff8
Bruno Alexandre <bruno.in.dk@gmail.com>
2021-11-29 21:08:25 +0100

    simple refactoring

--- lineMP.mjs
+++ lineMP.mjs
@@ -1,24 +1,22 @@
 function line(x0, y0, x1, y1) {
-	var dx = Math.abs(x1 - x0);
-	var dy = Math.abs(y1 - y0);
-	var sx = (x0 < x1) ? 1 : -1;
-	var sy = (y0 < y1) ? 1 : -1;
-	var err = dx - dy;
+	const dx = Math.abs(x1 - x0);
+	const dy = Math.abs(y1 - y0);
+	const sx = (x0 < x1) ? 1 : -1;
+	const sy = (y0 < y1) ? 1 : -1;
+	let err = dx - dy;
 
-	var result = [];
+	const result = [];
 
 	while (true) {
 		result.push({ x: x0, y: y0 });
 
-		if ((x0 === x1) && (y0 === y1)) break;
-		var e2 = 2 * err;
-		if (e2 > -dy) { err -= dy; x0 += sx; }
-		if (e2 < dx) { err += dx; y0 += sy; }
-	}
+	  if ((x0 === x1) && (y0 === y1)) break;
+	  const e2 = 2 * err;
+	  if (e2 > -dy) { err -= dy; x0 += sx; }
+	  if (e2 < dx) { err += dx; y0 += sy; }
+  }
 
 	return result;
 }
 
-export const lineMP = (a, b) => {
-	return line(a.x, a.y, b.x, b.y);
-}
+export const lineMP = (a, b) => line(a.x, a.y, b.x, b.y);
